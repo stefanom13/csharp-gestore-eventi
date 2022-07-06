3e66b0510abfcfa6f4db19e75d23008b09fd5a99
stefanom13 <stefanomancuso13@gmail.com>
2022-07-06 14:30:58 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Contesto e problema da risolvere:
+//Immaginate di lavorare in una software house, che ha diversi clienti.
+//Vi è stato commissionato da parte della vostra azienda la creazione di un gestionale eventi per eventi come concerti, conferenze, spettacoli,… per un suo cliente.
+//Il cliente necessita di un semplice programma senza interfaccia grafica (ossia che venga eseguito in console o terminale) che si occupa di:
+
+//Memorizzare e tenere traccia di tutti gli eventi in futuro che ha programmato
+//Poter gestire le prenotazioni e le disdette delle sue conferenze e tenere traccia quindi dei posti prenotati e di quelli disponibili per un dato evento
+//Poter gestire un intero programma di Eventi (ossia tenere traccia di tutti gli eventi che afferiscono ad serie di Conferenze)
+
